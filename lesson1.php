e514359aa12d9acdaf49ce0f70962355c36f511f
Kosinskiy Andrey <kosinskiy.andrey@ukr.net>
2020-04-23 02:50:49 +0300

    Added link to index.html to lesson3.html, this file has form what send get request to lessons3.php, it script return revert string from form and button for back.

--- lesson1.php
+++ lesson1.php
@@ -12,39 +12,42 @@
 <body>
   <header></header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.html">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Урок 1</a>
+        <li class="nav-item active">
+          <a class="nav-link" href="lesson1.php">Урок 1</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Урок 2</a>
+          <a class="nav-link" href="lesson2.php">Урок 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="lesson3.html">Урок 3</a>
         </li>
       </ul>
     </div>
   </nav>
   <main>
     <div class="container">
-      
-       
-          <?php
-          echo '<table class = "table table-bordered" ><tr>';
-          foreach (range(1, 10) as $first_multiplier) {
-            $table_formula = '';
-            foreach (range(1, 10) as $second_multiplier) {
-              $result = $first_multiplier * $second_multiplier;
-              $row_formula = "</p>" . $first_multiplier . ' X ' . $second_multiplier . ' = ' . $result . "</p>";
-              $table_formula .= $row_formula;
-            };
-            echo "<td>" . $table_formula . "</td>";
-            echo ($first_multiplier % 5 == 0) ? '</tr><tr>' : null;
-          };
-          echo '</tr></table>';
-          ?>
+
+
+      <?php
+      echo '<table class = "table table-bordered" ><tr>';
+      foreach (range(1, 10) as $first_multiplier) {
+        $table_formula = '';
+        foreach (range(1, 10) as $second_multiplier) {
+          $result = $first_multiplier * $second_multiplier;
+          $row_formula = "</p>" . $first_multiplier . ' X ' . $second_multiplier . ' = ' . $result . "</p>";
+          $table_formula .= $row_formula;
+        };
+        echo "<td>" . $table_formula . "</td>";
+        echo ($first_multiplier % 5 == 0) ? '</tr><tr>' : null;
+      };
+      echo '</tr></table>';
+      ?>
     </div>
   </main>
   <footer></footer>
